8a3e6f3b74d9356319aa3269095833c4a9689a4e
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-08 23:00:32 +0000

    update environment

--- resources/views/pages/dashboard.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    <div>
-        hello from dashboard
-    </div>
-@endsection
--- storage/framework/views/300398aa9cc61c39c7f94badc1e99256.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <div>
-        hello from dashboard
-    </div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\YouCode\Desktop\Med_el_Bachiri-oauth2-p2\resources\views/pages/dashboard.blade.php ENDPATH**/ ?>
